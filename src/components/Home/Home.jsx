17dd68d2cffbe63a70bae3993b99e75830a21258
Tobias <tobiasarganiaras12@hotmail.com>
2022-10-14 22:56:01 -0300

    home renderiza el nav

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from '../NavBar/NavBar'
+
+
+export default function Home() {
+  return (
+    <div className = 'homeContainer'>
+      <div className = 'navBar'>
+      <NavBar/>
+     </div>
+     </div>
+  );
+}
