24f060639dd18a01050c876a96a1e2d926777a76
villajul <villajul3@hotmail.comgit config --global user.name villajulgit config --global user.email villajul3@hotmail.com>
2022-10-16 03:36:26 -0300

    cardProduct

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Card = ({ images, name, price }) => {
   return (
-    <>
+    <article>
       <img src={images.filter((e) => e !== null)[0]} alt={name} />
       <h3>{name}</h3>
       <h3>{price}</h3>
       <button onClick={() => {}}>Add to cart</button>
-    </>
+    </article>
   )
 };
 
