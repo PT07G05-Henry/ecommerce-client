322ba9cc1171f293198e758338ac1eb697b72486
Christian Villegas <chrisvill.2312@gmai.com>
2022-10-16 21:55:48 +0000

    feat(client/redux): Updated API consumption

--- src/store/api.js
+++ src/store/api.js
@@ -2,15 +2,76 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 let initialState = {
+    categories: [{ toBeField: true }],
+    comments: [{ toBeField: true }],
+    deliveries: [{ toBeField: true }],
+    orders: [{ toBeField: true }],
+    payments: [{ toBeField: true }],
     products: [{ toBeField: true }],
     product: { noProduct: true },
+    users: [{ toBeField: true }],
     productsDisplay: [{ toBeField: true }]
 };
+
+export const getCategories = createAsyncThunk("api/getCategories", async () => {
+    try {
+        const response = await axios.get(
+            `http://${process.env.REACT_APP_DEV_API || document.domain}/categories`
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+export const getComments = createAsyncThunk("api/getComments", async () => {
+    try {
+        const response = await axios.get(
+            `http://${process.env.REACT_APP_DEV_API || document.domain}/comments`
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+export const getDeliveries = createAsyncThunk("api/getDeliveries", async () => {
+    try {
+        const response = await axios.get(
+            `http://${process.env.REACT_APP_DEV_API || document.domain}/deliveries`
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+export const getOrders = createAsyncThunk("api/getOrders", async () => {
+    try {
+        const response = await axios.get(
+            `http://${process.env.REACT_APP_DEV_API || document.domain}/orders`
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+export const getPayments = createAsyncThunk("api/getPayments", async () => {
+    try {
+        const response = await axios.get(
+            `http://${process.env.REACT_APP_DEV_API || document.domain}/payments`
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 export const getProducts = createAsyncThunk("api/getProducts", async () => {
     try {
         const response = await axios.get(
-            `http://${process.env.REACT_APP_DEV_API || document.domain}/product`
+            `http://${process.env.REACT_APP_DEV_API || document.domain}/products`
         );
         return response.data;
     } catch (error) {
@@ -21,7 +82,18 @@ export const getProducts = createAsyncThunk("api/getProducts", async () => {
 export const getProductById = createAsyncThunk("api/getProductById", async (id) => {
     try {
         const response = await axios.get(
-            `http://${process.env.REACT_APP_DEV_API || document.domain}/product/${id}`
+            `http://${process.env.REACT_APP_DEV_API || document.domain}/products/${id}`
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+export const getUsers = createAsyncThunk("api/getUsers", async () => {
+    try {
+        const response = await axios.get(
+            `http://${process.env.REACT_APP_DEV_API || document.domain}/users`
         );
         return response.data;
     } catch (error) {
@@ -34,41 +106,107 @@ export const apiSlice = createSlice({
     initialState,
     reducers: {
         start: (state) => {
+            state.categories = [{ toBeField: true }];
+            state.comments = [{ toBeField: true }];
+            state.deliveries = [{ toBeField: true }];
+            state.orders = [{ toBeField: true }];
+            state.payments = [{ toBeField: true }];
             state.products = [{ toBeField: true }];
             state.product = { noProduct: true };
+            state.users = [{ toBeField: true }];
             state.productsDisplay = [{ toBeField: true }];
         },
         display: (state) => {
             let products = state.products;
             //espacio para aplicar filtros y ordenamientos
-            state.productsDisplay=products;
+            state.productsDisplay = products;
         }
     },
     extraReducers: (builder) => {
         builder
+            .addCase(getCategories.pending, (state) => {
+                state.categories = [{ idle: true }];
+            })
+            .addCase(getCategories.rejected, (state) => {
+                state.categories = [{ error: "Something went wrong" }];
+            })
+            .addCase(getCategories.fulfilled, (state, action) => {
+                state.categories = action.payload;
+            })
+            .addCase(getComments.pending, (state) => {
+                state.comments = [{ idle: true }];
+            })
+            .addCase(getComments.rejected, (state) => {
+                state.comments = [{ error: "Something went wrong" }];
+            })
+            .addCase(getComments.fulfilled, (state, action) => {
+                state.comments = action.payload;
+            })
+            .addCase(getDeliveries.pending, (state) => {
+                state.deliveries = [{ idle: true }];
+            })
+            .addCase(getDeliveries.rejected, (state) => {
+                state.deliveries = [{ error: "Something went wrong" }];
+            })
+            .addCase(getDeliveries.fulfilled, (state, action) => {
+                state.deliveries = action.payload;
+            })
+            .addCase(getOrders.pending, (state) => {
+                state.orders = [{ idle: true }];
+            })
+            .addCase(getOrders.rejected, (state) => {
+                state.orders = [{ error: "Something went wrong" }];
+            })
+            .addCase(getOrders.fulfilled, (state, action) => {
+                state.orders = action.payload;
+            })
+            .addCase(getPayments.pending, (state) => {
+                state.payments = [{ idle: true }];
+            })
+            .addCase(getPayments.rejected, (state) => {
+                state.payments = [{ error: "Something went wrong" }];
+            })
+            .addCase(getPayments.fulfilled, (state, action) => {
+                state.payments = action.payload;
+            })
             .addCase(getProducts.pending, (state) => {
-                state.products=[{ idle: true }];
+                state.products = [{ idle: true }];
             })
             .addCase(getProducts.rejected, (state) => {
-                state.products = [{ error: "Something went wrong" }]; 
+                state.products = [{ error: "Something went wrong" }];
             })
             .addCase(getProducts.fulfilled, (state, action) => {
                 state.products = action.payload;
             })
             .addCase(getProductById.pending, (state) => {
-                state.product={ idle: true };
+                state.product = { idle: true };
             })
             .addCase(getProductById.rejected, (state) => {
-                state.product = { error: "Something went wrong" }; 
+                state.product = { error: "Something went wrong" };
             })
             .addCase(getProductById.fulfilled, (state, action) => {
                 state.product = action.payload;
+            })
+            .addCase(getUsers.pending, (state) => {
+                state.users = [{ idle: true }];
+            })
+            .addCase(getUsers.rejected, (state) => {
+                state.users = [{ error: "Something went wrong" }];
+            })
+            .addCase(getUsers.fulfilled, (state, action) => {
+                state.users = action.payload;
             });
     },
 });
 
+export const selectCategories = (state) => state.api.categories;
+export const selectComments = (state) => state.api.comments;
+export const selectDeliveries = (state) => state.api.deliveries;
+export const selectOrders = (state) => state.api.orders;
+export const selectPayments = (state) => state.api.payments;
 export const selectProducts = (state) => state.api.products;
 export const selectProduct = (state) => state.api.product;
+export const selectUsers = (state) => state.api.users;
 export const selectProductsDisplay = (state) => state.api.productsDisplay;
 
 export const {
