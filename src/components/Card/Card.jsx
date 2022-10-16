88a42b646617d7668deb12dc04d5d96813e21bdb
villajul <villajul3@hotmail.comgit config --global user.name villajulgit config --global user.email villajul3@hotmail.com>
2022-10-16 19:51:03 -0300

    ProductDetail

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Card = ({ id, images, name, price }) => {
+  const navigate = useNavigate();
   return (
-    <Link to={`/product/${id}`}>
-      <article>
-        <img src={images.filter((e) => e !== null)[0]} alt={name} />
-        <h3>{name}</h3>
-        <h3>{price}</h3>
-        <button onClick={() => {}}>Add to cart</button>
-      </article>
-    </Link>
+    <article onClick={() => navigate(`/products/${id}`)}>
+      <img src={images.filter((e) => e !== null)[0]} alt={name} />
+      <h3>{name}</h3>
+      <h3>{price}</h3>
+      <button onClick={() => {}}>Add to cart</button>
+    </article>
   );
 };
 
