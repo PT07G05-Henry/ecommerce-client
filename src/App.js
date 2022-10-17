bc45f6bda4f346f9ea19783fea82262b47c8d60a
villajul <villajul3@hotmail.comgit config --global user.name villajulgit config --global user.email villajul3@hotmail.com>
2022-10-16 22:37:09 -0300

    Productsview

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Welcome from "./containers/welcome/Welcome";
 import ProductDetail from "./components/ProductDetail/ProductDetail";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/products/:id" element={<ProductDetail />} />
       </Routes>
     </>
