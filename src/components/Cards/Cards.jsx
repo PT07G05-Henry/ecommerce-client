d80895a222a765821dc7d5bee2d9a41daa975302
Christian Villegas <chrisvill.2312@gmai.com>
2022-10-23 02:05:17 +0000

    feat(Client/Cards): Fix return to page 1, replaced old pager without compatibility whit changes on controler, fixed css

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -15,21 +15,32 @@ import {
   STOCK,
   RATING,
   USER_ROL_ID,
+  getCategories,
+  selectCategories,
 } from "../../store/api";
+import { useDispatch, useSelector } from "react-redux";
 import "./cards.css";
 import { ImFilter } from "react-icons/im";
 
 const REMOVE = "remove";
 
 const Cards = ({ products, dispatch }) => {
+  const internalDispatch = useDispatch();
   const [controller, setController] = useState(products.query);
+  const categoriesList = useSelector(selectCategories);
+  useEffect(() => {
+    (categoriesList[0].toBeField || categoriesList[0].error) &&
+      internalDispatch(getCategories());
+  }, [categoriesList]);
+
   const [arrayPag, setArrayPag] = useState([]);
   const [change, setChange] = useState(false);
   let buttons = [];
+
   const removeOfController = (key) => {
     let newController = structuredClone(controller);
     delete newController[key];
-    setController(newController);
+    setController({...newController,[PAGE]:"1"});
     setChange(!change);
   };
   const addToController = (flag) => {
@@ -37,150 +48,189 @@ const Cards = ({ products, dispatch }) => {
     setChange(!change);
   };
 
-  useEffect(() => {
-    change && dispatch(controller);
-    setArrayPag([]);
-    const array = [];
-    for (let i = 1; i <= products.totalPage; i++) {
-      array.push(i);
-    }
-    paginate(array, products.page, 5, products.totalPage);
-  }, [controller]);
-
-  //pager by Jorge ----
-  function paginate(array, pageNumber, separation, totalPage) {
-    if (totalPage - pageNumber < 5) {
-      setArrayPag(array.slice(-10));
-      return;
-    }
-    if (pageNumber < 5) {
-      setArrayPag(array.slice(0, 10));
-      return;
-    }
-    setArrayPag(
-      array.slice(
-        Number(pageNumber) - Number(separation) < 0
-          ? 0
-          : Number(pageNumber) - Number(separation),
-        Number(pageNumber) + Number(separation)
-      )
-    );
-  }
-  function pag(a) {
-    return (
-      <input
-        key={`page_${a}`}
-        flag={a}
-        id={"paginadoNum" + a}
-        className={`${Number(products.page) === Number(a) ? "active" : ""} btn`}
-        type="button"
-        onClick={(e) => todo(e)} //cambiar esto
-        value={a}
-        disabled={products.page === a.toString()}
-      />
-    );
-  }
-  function todo(e) {
-    if (e.target.value === "PrevPag") {
-      window.location.hash = `#${products.page}`;
-      dispatch(products.prev);
-      return;
-      // setCurrentPage(products.page - 1)
-    }
-    if (e.target.value === "NextPag") {
-      window.location.hash = `#${products.page}`;
-      dispatch(products.next);
-      return;
-    }
-    window.location.hash = `#${e.target.value}`;
-    const flag = { [PAGE]: e.target.value.toString() };
-    addToController(flag);
-    // setCurrentPage(Number(e.target.value))
-  }
-  //-------------------
-
-  const quantity = {
-    name: "Products per page",
-    key: QUANTITY,
-    values: [
-      { [QUANTITY]: "25", label: "25 Items" },
-      { [QUANTITY]: "50", label: "50 Items" },
-      { [QUANTITY]: "75", label: "75 Items" },
-      { [QUANTITY]: "100", label: "100 Items" },
-    ],
-  };
-
-  const typeOrder = {
-    name: "Order direction",
-    key: TYPE_ORDER,
-    values: [
-      { [TYPE_ORDER]: ASC, label: "Ascendent" },
-      { [TYPE_ORDER]: DESC, label: "Decedent" },
-    ],
-  };
-
-  const orderBy = {
-    name: "Order by",
-    key: ORDERBY,
-    values: [
-      { [ORDERBY]: ID, label: "Id" },
-      { [ORDERBY]: NAME, label: "Name" },
-      { [ORDERBY]: PRICE, label: "Price" },
-      { [ORDERBY]: DESCRIPTION, label: "Description" },
-      { [ORDERBY]: STOCK, label: "Stock" },
-      { [ORDERBY]: RATING, label: "Rating" },
-      { [ORDERBY]: USER_ROL_ID, label: "CreatedBy" },
-    ],
-  };
-
-  const filters = [quantity, typeOrder, orderBy];
-
   const isInController = (key) => {
     const applied = Object.keys(controller);
     return applied.length ? Boolean(applied.find((e) => e === key)) : false;
   };
 
-  const applyFilter = (e, key) => {
-    const flag = JSON.parse(e.target.value);
-    flag[key] === REMOVE ? removeOfController(key) : addToController(flag);
-  };
+  useEffect(() => {
+    change && dispatch(controller);
+  }, [controller]);
 
-  const Filter = ({ name, flag, values }) => {
+  //Pager------------------------
+
+  const Pager = () => {
+    const NEXT = true;
+    const BACK = false;
+    const pageButtons = (direction) => {
+      const DEFAULT = "cards__pager-btn btn";
+      const SM = "cards__pager-btn-sm cards__pager-btn btn";
+      const MD = "cards__pager-btn-md cards__pager-btn btn";
+      const LG = "cards__pager-btn-lg cards__pager-btn btn";
+      const currentPage = parseInt(controller.page);
+      let pages = [
+        {
+          class: DEFAULT,
+          page: (direction ? 1 : -1) + currentPage,
+        },
+        {
+          class: SM,
+          page: (direction ? 2 : -2) + currentPage,
+        },
+        {
+          class: MD,
+          page: (direction ? 3 : -3) + currentPage,
+        },
+        {
+          class: MD,
+          page: direction ? Math.ceil((currentPage + 10) / 10) * 10 : Math.floor((currentPage - 10) / 10) * 10,
+        },
+        {
+          class: LG,
+          page: direction ? Math.ceil((currentPage + 20) / 10) * 10 : Math.floor((currentPage - 20) / 10) * 10,
+        },
+      ];
+      if (direction) {
+        let button = {
+          class: LG,
+          page:
+            Math.ceil(
+              (currentPage + (products.totalPage - currentPage) / 2) / 10
+            ) * 10,
+        };
+        button.page > pages[4].page && pages.push(button);
+      } else {
+        let button = {
+          class: LG,
+          page: Math.floor((currentPage - (currentPage / 2)) / 10) * 10,
+        };
+        button.page < pages[4].page && pages.push(button);
+      }
+      pages = pages.filter((button) =>
+        direction ? button.page < products.totalPage : button.page > 1
+      );
+      direction
+        ? currentPage !== products.totalPage &&
+          pages.push({ class: DEFAULT, page: products.totalPage })
+        : currentPage !== 1 && pages.push({ class: DEFAULT, page: 1 });
+      return pages.map((button) => ({ ...button, page: `${button.page}` }));
+    };
+    const PageButton = ({ page }) => (
+      <button
+        className={page.class}
+        onClick={() => {
+          addToController({ [PAGE]: page.page });
+        }}
+      >
+        {page.page}
+      </button>
+    );
+
     return (
-      <label className="filter">
-        {name}
-        <select
-          value={
-            isInController(flag)
-              ? JSON.stringify({ [flag]: controller[flag] })
-              : JSON.stringify({ [flag]: REMOVE })
-          }
-          onChange={(e) => applyFilter(e, flag)}
-        >
-          <option
-            value={JSON.stringify({ [flag]: REMOVE })}
-            className="filter__option"
-          >
-            None
-          </option>
-          {values.map((option) => {
-            return (
-              <option
-                key={`option_${flag}-${option[flag]}`}
-                value={JSON.stringify({ [flag]: option[flag] })}
-                className="filter__option"
-              >
-                {option.label}
-              </option>
-            );
-          })}
-        </select>
-      </label>
+      <div className="cards__pager">
+        <div className="cards__pager-back">
+          {pageButtons(BACK).map((page) => (
+            <PageButton page={page} />
+          ))}
+        </div>
+        <div className="btn cards__pager-currentPage">{controller.page}</div>
+        <div className="cards__pager-next">
+          {pageButtons(NEXT).map((page) => (
+            <PageButton page={page} />
+          ))}
+        </div>
+      </div>
     );
   };
 
-  const Filters = ({ filters }) => {
+  //Filtros----------------------
+
+  const filters = [
+    {
+      name: "Categories",
+      key: CATEGORY,
+      values: !(categoriesList[0].toBeField || categoriesList[0].error)
+        ? categoriesList.map((category) => ({
+            [CATEGORY]: category.id,
+            label: category.name,
+          }))
+        : [{ [CATEGORY]: REMOVE, label: "Loading Categories" }],
+    },
+    {
+      name: "Products per page",
+      key: QUANTITY,
+      values: [
+        { [QUANTITY]: "25", label: "25 Items" },
+        { [QUANTITY]: "50", label: "50 Items" },
+        { [QUANTITY]: "75", label: "75 Items" },
+        { [QUANTITY]: "100", label: "100 Items" },
+      ],
+    },
+    {
+      name: "Order direction",
+      key: TYPE_ORDER,
+      values: [
+        { [TYPE_ORDER]: ASC, label: "Ascendent" },
+        { [TYPE_ORDER]: DESC, label: "Decedent" },
+      ],
+    },
+    {
+      name: "Order by",
+      key: ORDERBY,
+      values: [
+        { [ORDERBY]: ID, label: "Id" },
+        { [ORDERBY]: NAME, label: "Name" },
+        { [ORDERBY]: PRICE, label: "Price" },
+        { [ORDERBY]: DESCRIPTION, label: "Description" },
+        { [ORDERBY]: STOCK, label: "Stock" },
+        { [ORDERBY]: RATING, label: "Rating" },
+        { [ORDERBY]: USER_ROL_ID, label: "CreatedBy" },
+      ],
+    },
+  ];
+
+  const RenderFilters = ({ filters }) => {
     const [show, setShow] = useState(false);
+
+    const Filter = ({ name, flag, values }) => {
+      const applyFilter = (e, key) => {
+        const flag = {...JSON.parse(e.target.value),[PAGE]:"1"};
+        flag[key] === REMOVE ? removeOfController(key) : addToController(flag);
+      };
+
+      return (
+        <label className="filter">
+          {name}
+          <select
+            value={
+              isInController(flag)
+                ? JSON.stringify({ [flag]: `${controller[flag]}` })
+                : JSON.stringify({ [flag]: REMOVE })
+            }
+            onChange={(e) => applyFilter(e, flag)}
+          >
+            <option
+              value={JSON.stringify({ [flag]: REMOVE })}
+              className="filter__option"
+            >
+              None
+            </option>
+            {values.map((option) => {
+              return (
+                <option
+                  key={`option_${flag}-${option[flag]}`}
+                  value={JSON.stringify({ [flag]: `${option[flag]}` })}
+                  className="filter__option"
+                >
+                  {option.label}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      );
+    };
+
     return (
       <>
         <button
@@ -212,65 +262,12 @@ const Cards = ({ products, dispatch }) => {
     );
   };
 
+  //Render-----------------------
+
   return (
     <>
-      <div className="divCenter">
-        <p className="btn">page: {products.page}</p>
-        <div className="cards__controller">
-          <div className="cards__controller-filters"></div>
-          <div className="cards__controller-pager">
-            {/* botton prev */}
-            <button
-              className="btn"
-              value="PrevPag"
-              onClick={(e) => {
-                todo(e);
-              }}
-              style={!products.prev ? { display: "none" } : undefined}
-            >
-              {"<"}
-            </button>
-
-            {/* {buttons} */}
-            {/* paginado numerado */}
-            {products.page > 5 && pag(1)}
-            {products.page > 5 && (
-              <input
-                type="button"
-                className="btnDots"
-                value={"..."}
-                disabled
-              ></input>
-            )}
-
-            {arrayPag.length !== 0 &&
-              arrayPag.map((a) => {
-                return pag(a);
-              })}
-            {products.totalPage - products.page > 5 && (
-              <input
-                type="button"
-                className="btnDots"
-                value={"..."}
-                disabled
-              ></input>
-            )}
-            {products.totalPage - products.page > 5 && pag(products.totalPage)}
-            {/* botton next */}
-            <button
-              className="btn"
-              value="NextPag"
-              onClick={(e) => {
-                todo(e);
-              }}
-              style={!products.next ? { display: "none" } : undefined}
-            >
-              {">"}
-            </button>
-          </div>
-        </div>
-      </div>
-      <Filters filters={filters} />
+      <Pager />
+      <RenderFilters filters={filters} />
       <div className="cards__grid">
         {products.results?.map((el) => (
           <Card
