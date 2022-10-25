f13558bc6458ee59162abc548fbb8f93ea907d00
Christian Villegas <chrisvill.2312@gmai.com>
2022-10-25 02:33:01 +0000

    Feat(client/Redux): Added getThisUser to redux

--- src/components/Account/ButtonLogin.jsx
+++ src/components/Account/ButtonLogin.jsx
@@ -3,11 +3,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LOGO from "../../assets/Logo.svg";
 
 const ButtonLogin = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithPopup } = useAuth0();
   return (
     <div className="nav__user-image">
       <div className="nav__logo">
-        <img src={LOGO} alt="logo" onClick={() => loginWithRedirect()} />
+        <img src={LOGO} alt="logo" onClick={() => loginWithPopup()} />
       </div>
     </div>
   );
