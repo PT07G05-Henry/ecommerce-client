a561c16f2623ba17f3b6ae88ad624f09353335be
mativillagra98 <mativillagra98@gmail.com>
2022-11-04 20:51:36 -0300

    fix para el historial de ordenes de usuario

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Paginated from "./Paginated.jsx";
 import { getOrders, selectOrders } from "../../store/api";
+import { Link } from "react-router-dom";
+import Loading from "../loading/Loading.jsx";
 
 export default function History() {
   const dispatch = useDispatch();
@@ -26,23 +28,39 @@ export default function History() {
     setFilteredOrders(orders)
   },[orders])
 
-  return (
-    <>
-      <div>
-        <select onChange={handleChange}>
-          <option value="All">All</option>
-          <option value="Pending">Pending</option>
-          <option value="Accepted">Accepted</option>
-          <option value="Rejected">Rejected</option>
-          <option value="Dispatched">Dispatched</option>
-          <option value="Billed">Billed</option>
-        </select>
-        <Paginated
-          filteredOrders={filteredOrders}
-          setFilteredOrders={setFilteredOrders}
-          orders={orders}
-        />
-      </div>
-    </>
-  );
+  if(typeof(orders[0]) === "object") {
+    return (<Loading/>)
+  }
+  else if(orders.length) {
+    return (
+      <>
+        <div>
+          <select onChange={handleChange}>
+            <option value="All">All</option>
+            <option value="Pending">Pending</option>
+            <option value="Accepted">Accepted</option>
+            <option value="Rejected">Rejected</option>
+            <option value="Dispatched">Dispatched</option>
+            <option value="Billed">Billed</option>
+          </select>
+          <Paginated
+            filteredOrders={filteredOrders}
+            setFilteredOrders={setFilteredOrders}
+            orders={orders}
+          />
+        </div>
+      </>
+    );
+  } 
+  else {
+    return (
+      <>
+        <div>
+          <h1>You must make a purchase to view order history</h1>
+          <Link to="/catalog"><h2 className="btn primary">Make a Purchase</h2></Link>
+        </div>
+      </>
+    )
+  }
+  
 }
