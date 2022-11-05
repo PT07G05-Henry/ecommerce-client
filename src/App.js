824f4fea53c198a089de59bd5013b872a22e6eab
Profesorjand <jorge.andrade.y@hotmail.com>
2022-11-05 05:49:06 -0300

    feat / cart cuando log user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import WebFrame from "./containers/webFrame/WebFrame";
 import Home from "./containers/Home/Home";
@@ -11,14 +11,20 @@ import OrderDetail from "../src/components/OrderDetail/OrderDetail";
 import DashBoard from "./components/DashBoard/DashBoard";
 import Cart from "../src/components/Cart/Cart";
 import Orders from "../src/components/Orders/Orders";
-import Payment from "../src/components/Payment/Payment"
-import { selectThisUserRoles } from "./store/thisUser";
-import { useSelector } from "react-redux";
+import Payment from "../src/components/Payment/Payment";
+import { selectThisUserRoles, selectThisUser } from "./store/thisUser";
+import { useDispatch, useSelector } from "react-redux";
 import Redirect from "./components/Redirect/Redirect";
 import About from "./components/AboutUs/Aboutus";
+import api, { endPoint } from "./lib/api";
+import { selectCarts, dataBaseValue } from "./store/cart";
+
 function App() {
   const navigate = useNavigate();
+  const cart = useSelector(selectCarts);
   const rol = useSelector(selectThisUserRoles);
+  const user = useSelector(selectThisUser);
+  const dispatch = useDispatch();
   const access = () => {
     if (rol.find((e) => e === "Superadmin")) {
       return "Superadmin";
@@ -33,12 +39,65 @@ function App() {
       return "Guest";
     }
   };
+
+  useEffect(() => {
+    if (rol[0] === "User") {
+      cart.length > 0
+        ? api
+            .post(endPoint.cart, {
+              data: { userId: user.userDb.id, products: cart },
+            })
+            .then((r) => {
+            if(!areEqual(cart,r.data.items)){
+              //hacer dispatch a cart con los valores de la Data Base
+              dispatch(dataBaseValue(r.data.items))
+            }
+
+            }
+            )
+        : api.post(endPoint.cart, { data: { userId: user.userDb.id } }).then((r) => {
+          if(!areEqual(cart,r.data.items)){
+            //hacer dispatch a cart con los valores de la Data Base
+            dispatch(dataBaseValue(r.data.items))
+          }
+          });
+      return;
+    }
+  }, [rol]);
+
+  useEffect(()=>{
+    if (rol[0] === "User") {
+      if(cart.length > 0){
+        api.post(endPoint.cart, {data: { userId: user.userDb.id } }).then((r)=>{
+          if(!areEqual(cart,r.data.items))
+          return api.put(endPoint.cart, {data: {userId:user.userDb.id, products:cart}}).then(t=>console.log(t))
+        })
+      }else{
+        api.delete(endPoint.cart, {data: {userId: user.userDb.id}})
+      }
+    }
+  },[cart])
+
+  function areEqual(array1, array2) {
+    if (array1.length === array2.length) {
+      return array1.every((element, index) => {
+        if (element === array2[index]) {
+          return true;
+        }
+        return false;
+      });
+    }
+    return false;
+  }
+
   return (
     <>
       <Routes>
         <Route path="/" element={<WebFrame />}>
           <Route index element={<Home />} />
-          {!(access()=== "Admin") && <Route path="/payment" element={<Payment />} />}
+          {!(access() === "Admin") && (
+            <Route path="/payment" element={<Payment />} />
+          )}
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/productsByName/:name" element={<SearchByName />} />
           <Route path="/products/:id" element={<ProductDetail />} />
