2cce7088c76ef84ede69d04d9de27bccb60dc863
Christian Villegas <chrisvill.2312@gmai.com>
2022-11-05 07:29:50 +0000

    WIP(Client/Style): some components styled - Feat(Clinet/Protection): New component and logic for routing and component protection

--- src/App.js
+++ src/App.js
@@ -1,64 +1,53 @@
 import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import WebFrame from "./containers/webFrame/WebFrame";
 import Home from "./containers/Home/Home";
 import Catalog from "./containers/catalog/Catalog";
 import ProductDetail from "./components/ProductDetail/ProductDetail";
-import CreateProduct from "../src/components/CreateProduct/CreateProduct";
 import SearchByName from "./containers/SearchByName/SearchByName";
 import UpdateProduct from "../src/components/UpdateProduct/UpdateProduct";
 import OrderDetail from "../src/components/OrderDetail/OrderDetail";
 import DashBoard from "./components/DashBoard/DashBoard";
 import Cart from "../src/components/Cart/Cart";
-import Orders from "../src/components/Orders/Orders";
 import Payment from "../src/components/Payment/Payment"
-import { selectThisUserRoles } from "./store/thisUser";
-import { useSelector } from "react-redux";
 import Redirect from "./components/Redirect/Redirect";
 import About from "./components/AboutUs/Aboutus";
+import ProtectedFrom from "./components/protectedFrom/ProtectedFrom";
+import Profile from "./components/Profile/Profile";
+import History from "./components/History/History";
+import Products from "./components/Products/Products";
+import CreateProduct from "./components/CreateProduct/CreateProduct";
+import CreateCategory from "./components/CreateCategory/CreateCategory";
+import Orders from "../src/components/Orders/Orders";
+import Users from "./components/Users/Users";
+import { selectThisUserId } from "./store/thisUser";
+import { useSelector } from "react-redux";
+import access from "./lib/access";
+
 function App() {
-  const navigate = useNavigate();
-  const rol = useSelector(selectThisUserRoles);
-  const access = () => {
-    if (rol.find((e) => e === "Superadmin")) {
-      return "Superadmin";
-    }
-    if (rol.find((e) => e === "Admin")) {
-      return "Admin";
-    }
-    if (rol.find((e) => e === "User")) {
-      return "User";
-    }
-    if (rol.find((e) => e === "Guest")) {
-      return "Guest";
-    }
-  };
+  const userId = useSelector(selectThisUserId);
   return (
     <>
       <Routes>
         <Route path="/" element={<WebFrame />}>
           <Route index element={<Home />} />
-          {!(access()=== "Admin") && <Route path="/payment" element={<Payment />} />}
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/productsByName/:name" element={<SearchByName />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/aboutus" element={<About />} />
-          {!(access() === ("Guest" || "User")) && (
-            <Route path="/create/product" element={<CreateProduct />} />
-          )}
-          {!(access() === ("Guest" || "User" || "Admin")) && (
-            <Route path="/or" element={<Orders />} />
-          )}
-          {!(access() === ("Guest" || "User" || "Admin")) && (
-            <Route path="orDetail/:id" element={<OrderDetail />} />
-          )}
-          {!(access() === "Admin") && <Route path="/cart" element={<Cart />} />}
-          {!(access() === ("Guest" || "User" || "Admin")) && (
-            <Route path="/update/product/:id" element={<UpdateProduct />} />
-          )}
-          {!(access() === "Guest") && (
-            <Route path="/dashBoard" element={<DashBoard />} />
-          )}
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="productsByName/:name" element={<SearchByName />} />
+          <Route path="products/:id" element={<ProductDetail />} />
+          <Route path="aboutus" element={<About />} />
+          <Route path="payment" element={<ProtectedFrom Admin><Payment /></ProtectedFrom>} />
+          <Route path="orDetail/:id" element={<ProtectedFrom Guest User Admin><OrderDetail /></ProtectedFrom>} />
+          <Route path="cart" element={<ProtectedFrom Admin><Cart /></ProtectedFrom>} />
+          <Route path="update/product/:id" element={<ProtectedFrom Guest User Admin><UpdateProduct /></ProtectedFrom>} />
+          <Route path="dashBoard" element={<ProtectedFrom Guest><DashBoard /></ProtectedFrom>} >
+            <Route index element={<ProtectedFrom Guest ><Profile rol={access()} userId={userId} test={"test"} /></ProtectedFrom>} />
+            <Route path="history" element={<ProtectedFrom Guest Admin Superadmin><History /></ProtectedFrom>} />
+            <Route path="products" element={<ProtectedFrom Guest User>{access() === "User" ? <Profile rol={access()} userId={userId} test={"test"} /> : <Products rol={access()} adminId={userId} />}</ProtectedFrom>} />
+            <Route path="createProduct" element={<ProtectedFrom Guest User><CreateProduct rol={access()} adminId={userId} /></ProtectedFrom>} />
+            <Route path="createCategory" element={<ProtectedFrom Guest User><CreateCategory /></ProtectedFrom>} />
+            <Route path="orders" element={<ProtectedFrom Guest User> <Orders rol={access()} adminId={userId} /></ProtectedFrom>} />
+            <Route path="users" element={<ProtectedFrom Guest User Admin><Users /></ProtectedFrom>} />
+          </Route>
           <Route path="*" element={<Redirect />} />
         </Route>
       </Routes>
--- src/components/SeeProducts/SeeProduct.jsx
+++ src/components/SeeProducts/SeeProduct.jsx
@@ -1,12 +1,9 @@
-import axios from "axios";
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import api, { endPoint, query, value } from "../../lib/api";
 import Card from "../Card/Card";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, EffectCoverflow } from "swiper";
-import { getThisUser, selectThisUserRoles } from "../../store/thisUser";
-import { useAuth0 } from "@auth0/auth0-react";
+import { selectThisUserRoles } from "../../store/thisUser";
 import { useDispatch, useSelector } from "react-redux";
 import "./SeeProduct.css";
 import "swiper/css";
@@ -15,27 +12,89 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import { getProducts, CATEGORY } from "../../store/products";
 import { useNavigate } from "react-router-dom";
+import Loading from "../loading/Loading";
 
 const SeeProduct = ({ category, name }) => {
   const [products, setProducts] = useState([]);
-  const [loaded, setLoaded] = useState(false);
   const rol = useSelector(selectThisUserRoles);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   useEffect(() => {
-    !loaded &&
-      axios
+    !products.length &&
+      api
         .get(
-          `https://${
-            process.env.REACT_APP_DEV_API || document.domain
-          }/products?quantity=5&orderBy=rating&typeOrder=DESC&category=${category}`
+          endPoint.products,
+          {
+            params: {
+              [query.quantity]: 5,
+              [query.orderBy]: value.rating,
+              [query.typeOrder]: value.DESC,
+              [query.category]: category,
+            },
+          } // quantity=5&orderBy=rating&typeOrder=DESC&category=${category}`
         )
         .then((res) => {
           setProducts(res.data.results);
         });
-  }, [loaded]);
+  }, [products]);
 
-
+  const Carousel = () => (
+    <div className="seeProduct__container">
+      <Swiper
+        loop={true}
+        effect={"coverflow"}
+        slidesPerView={3}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        breakpoints={{
+          300: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          360: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        navigation={true}
+        modules={[Navigation, EffectCoverflow]}
+        grabCursor={true}
+        className="Card-Product"
+      >
+        {products?.map((el) => (
+          <SwiperSlide key={el.id}>
+            <Card
+              key={el.id}
+              id={el.id}
+              images={el.images}
+              name={el.name}
+              price={el.price}
+              stock={el.stock}
+              description={el.description}
+              rol={rol}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 
   return (
     <div className="title">
@@ -48,61 +107,13 @@ const SeeProduct = ({ category, name }) => {
       >
         {name}
       </h1>
-      <div className="seeProduct__container">
-        <Swiper
-          loop={true}
-          effect={"coverflow"}
-          slidesPerView={3}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          breakpoints={{
-            300: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            360: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            640: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 50,
-            },
-          }}
-          navigation={true}
-          modules={[Navigation, EffectCoverflow]}
-          grabCursor={true}
-          className="Card-Product"
-        >
-          {products?.map((el) => (
-            <SwiperSlide key={el.id}>
-              <Card
-                key={el.id}
-                id={el.id}
-                images={el.images}
-                name={el.name}
-                price={el.price}
-                stock={el.stock}
-                description={el.description}
-                rol={rol}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      {!products.length ? (
+        <div className="seeProduct__carousel">
+          <Loading />
+        </div>
+      ) : (
+        <Carousel />
+      )}
     </div>
   );
 };
