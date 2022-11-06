4a0e0aaa72daa0815b2ca40b385b208dc490b0e9
Profesorjand <jorge.andrade.y@hotmail.com>
2022-11-06 19:15:10 -0300

    permanencia de cart como invitado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import WebFrame from "./containers/webFrame/WebFrame";
 import Home from "./containers/Home/Home";
@@ -16,7 +16,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Redirect from "./components/Redirect/Redirect";
 import About from "./components/AboutUs/Aboutus";
 import api, { endPoint } from "./lib/api";
-import { selectCarts, dataBaseValue } from "./store/cart";
+import { selectCarts, dataBaseValue, reset } from "./store/cart";
 
 import ProtectedFrom from "./components/protectedFrom/ProtectedFrom";
 import Profile from "./components/Profile/Profile";
@@ -34,44 +34,62 @@ function App() {
   const rol = useSelector(selectThisUserRoles);
   const user = useSelector(selectThisUser);
   const dispatch = useDispatch();
+  const [onlyOnce, setOnlyOnce] = useState(true)
 
   useEffect(() => {
+    if (rol[0] === "Guest") {
+        const cartStore = JSON.parse(window.localStorage.getItem("cart"));
+        onlyOnce &&
+        cartStore?.length > 0 && dispatch(dataBaseValue(cartStore)) ;
+        setOnlyOnce(false);
+    }
+
     if (rol[0] === "User") {
+      window.localStorage.setItem("cart", JSON.stringify([]))
       cart.length > 0
         ? api
             .post(endPoint.cart, {
               data: { userId: user.userDb.id, products: cart },
             })
             .then((r) => {
-            if(!areEqual(cart,r.data.items)){
-              //hacer dispatch a cart con los valores de la Data Base
-              dispatch(dataBaseValue(r.data.items))
-            }
-
-            }
-            )
-        : api.post(endPoint.cart, { data: { userId: user.userDb.id } }).then((r) => {
-          if(!areEqual(cart,r.data.items)){
-            //hacer dispatch a cart con los valores de la Data Base
-            dispatch(dataBaseValue(r.data.items))
-          }
-          });
+              if (!areEqual(cart, r.data.items)) {
+                //hacer dispatch a cart con los valores de la Data Base
+                dispatch(dataBaseValue(r.data.items));
+              }
+            })
+        : api
+            .post(endPoint.cart, { data: { userId: user.userDb.id } })
+            .then((r) => {
+              if (!areEqual(cart, r.data.items)) {
+                //hacer dispatch a cart con los valores de la Data Base
+                dispatch(dataBaseValue(r.data.items));
+              }
+            });
       return;
     }
   }, [rol]);
 
-  useEffect(()=>{
+  useEffect(() => {
+    if(rol[0] === "Guest"){
+      window.localStorage.setItem("cart", JSON.stringify(cart))
+    }
+
     if (rol[0] === "User") {
-      if(cart.length > 0){
-        api.post(endPoint.cart, {data: { userId: user.userDb.id } }).then((r)=>{
-          if(!areEqual(cart,r.data.items))
-          return api.put(endPoint.cart, {data: {userId:user.userDb.id, products:cart}}).then(t=>console.log(t))
-        })
-      }else{
-        api.delete(endPoint.cart, {data: {userId: user.userDb.id}})
+      if (cart.length > 0) {
+        api
+          .post(endPoint.cart, { data: { userId: user.userDb.id } })
+          .then((r) => {
+            if (!areEqual(cart, r.data.items))
+              return api
+                .put(endPoint.cart, {
+                  data: { userId: user.userDb.id, products: cart },
+                })
+          });
+      } else {
+        api.delete(endPoint.cart, { data: { userId: user.userDb.id } });
       }
     }
-  },[cart])
+  }, [cart]);
 
   function areEqual(array1, array2) {
     if (array1.length === array2.length) {
@@ -94,18 +112,107 @@ function App() {
           <Route path="productsByName/:name" element={<SearchByName />} />
           <Route path="products/:id" element={<ProductDetail />} />
           <Route path="aboutus" element={<About />} />
-          <Route path="payment" element={<ProtectedFrom Admin><Payment /></ProtectedFrom>} />
-          <Route path="orDetail/:id" element={<ProtectedFrom Guest User Admin><OrderDetail /></ProtectedFrom>} />
-          <Route path="cart" element={<ProtectedFrom Admin><Cart /></ProtectedFrom>} />
-          <Route path="update/product/:id" element={<ProtectedFrom Guest User Admin><UpdateProduct /></ProtectedFrom>} />
-          <Route path="dashBoard" element={<ProtectedFrom Guest><DashBoard /></ProtectedFrom>} >
-            <Route index element={<ProtectedFrom Guest ><Profile rol={access()} userId={userId} test={"test"} /></ProtectedFrom>} />
-            <Route path="history" element={<ProtectedFrom Guest Admin Superadmin><History /></ProtectedFrom>} />
-            <Route path="products" element={<ProtectedFrom Guest User>{access() === "User" ? <Profile rol={access()} userId={userId} test={"test"} /> : <Products rol={access()} adminId={userId} />}</ProtectedFrom>} />
-            <Route path="createProduct" element={<ProtectedFrom Guest User><CreateProduct rol={access()} adminId={userId} /></ProtectedFrom>} />
-            <Route path="createCategory" element={<ProtectedFrom Guest User><CreateCategory /></ProtectedFrom>} />
-            <Route path="orders" element={<ProtectedFrom Guest User> <Orders rol={access()} adminId={userId} /></ProtectedFrom>} />
-            <Route path="users" element={<ProtectedFrom Guest User Admin><Users /></ProtectedFrom>} />
+          <Route
+            path="payment"
+            element={
+              <ProtectedFrom Admin>
+                <Payment />
+              </ProtectedFrom>
+            }
+          />
+          <Route
+            path="orDetail/:id"
+            element={
+              <ProtectedFrom Guest User Admin>
+                <OrderDetail />
+              </ProtectedFrom>
+            }
+          />
+          <Route
+            path="cart"
+            element={
+              <ProtectedFrom Admin>
+                <Cart />
+              </ProtectedFrom>
+            }
+          />
+          <Route
+            path="update/product/:id"
+            element={
+              <ProtectedFrom Guest User Admin>
+                <UpdateProduct />
+              </ProtectedFrom>
+            }
+          />
+          <Route
+            path="dashBoard"
+            element={
+              <ProtectedFrom Guest>
+                <DashBoard />
+              </ProtectedFrom>
+            }
+          >
+            <Route
+              index
+              element={
+                <ProtectedFrom Guest>
+                  <Profile rol={access()} userId={userId} test={"test"} />
+                </ProtectedFrom>
+              }
+            />
+            <Route
+              path="history"
+              element={
+                <ProtectedFrom Guest Admin Superadmin>
+                  <History />
+                </ProtectedFrom>
+              }
+            />
+            <Route
+              path="products"
+              element={
+                <ProtectedFrom Guest User>
+                  {access() === "User" ? (
+                    <Profile rol={access()} userId={userId} test={"test"} />
+                  ) : (
+                    <Products rol={access()} adminId={userId} />
+                  )}
+                </ProtectedFrom>
+              }
+            />
+            <Route
+              path="createProduct"
+              element={
+                <ProtectedFrom Guest User>
+                  <CreateProduct rol={access()} adminId={userId} />
+                </ProtectedFrom>
+              }
+            />
+            <Route
+              path="createCategory"
+              element={
+                <ProtectedFrom Guest User>
+                  <CreateCategory />
+                </ProtectedFrom>
+              }
+            />
+            <Route
+              path="orders"
+              element={
+                <ProtectedFrom Guest User>
+                  {" "}
+                  <Orders rol={access()} adminId={userId} />
+                </ProtectedFrom>
+              }
+            />
+            <Route
+              path="users"
+              element={
+                <ProtectedFrom Guest User Admin>
+                  <Users />
+                </ProtectedFrom>
+              }
+            />
           </Route>
           <Route path="*" element={<Redirect />} />
         </Route>
