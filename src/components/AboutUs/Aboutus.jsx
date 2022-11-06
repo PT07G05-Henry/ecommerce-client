9161aa42fdc1eb4bfb600d5416e26ab0a1468626
villajul <villajul3@hotmail.comgit config --global user.name villajulgit config --global user.email villajul3@hotmail.com>
2022-11-06 03:51:13 -0300

    'stiledProfileAndMore'

--- src/components/AboutUs/Aboutus.jsx
+++ src/components/AboutUs/Aboutus.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Aboutus.css"; 
+import "./Aboutus.css";
 import dat from "../../img/dat.jpg";
 import chrs from "../../img/chrs.jpg";
 import fran from "../../img/fran.jpg";
@@ -8,111 +8,110 @@ import jor from "../../img/jor.jpg";
 import fede from "../../img/fede.jpg";
 import julio from "../../img/julio.jpg";
 
-
-
-
-
-
 const nos = [
-   {
-        name: "Franco",
-        profession: "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
-        mail: "frankitooserafini493@gmail.com",
-        image: fran,
-        age:26
-       
-    },{
-        name: "Jorge",
-        profession: "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
-        mail: "jorge.andrade.y@hotmail.com",
-        image: jor,
-        age:30
-       
-    },{
-        name: "Fede",
-        profession: "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
-        mail: "ing.finterlandi@gmail.com",
-        image: fede,
-        age:26
-       
-    },
-    {
-        name: "Chris",
-        profession: "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
-        mail: "chrisvill.2312@gmail.com",
-        image: chrs,
-        age:26
-       
-    },
-    {
-        name: "Julio",
-        profession: "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
-        mail: "villajul3@hotmail.com",
-        image: julio,
-        age:26
-       
-    },
-    {
-        name: "Matias",
-        profession: "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
-        mail: "mativillagra98@gmail.com",
-        image: matias,
-        age:26
-       
-    },
-    {
-        name: "Ramiro",
-        profession: "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
-        mail: "ramijujuy@gmail.com",
-        image: dat,
-        age:47
-       
-    }]
+  {
+    name: "Franco",
+    profession:
+      "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
+    mail: "frankitooserafini493@gmail.com",
+    image: fran,
+    age: 26,
+  },
+  {
+    name: "Jorge",
+    profession:
+      "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
+    mail: "jorge.andrade.y@hotmail.com",
+    image: jor,
+    age: 30,
+  },
+  {
+    name: "Fede",
+    profession:
+      "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
+    mail: "ing.finterlandi@gmail.com",
+    image: fede,
+    age: 26,
+  },
+  {
+    name: "Chris",
+    profession:
+      "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
+    mail: "chrisvill.2312@gmail.com",
+    image: chrs,
+    age: 26,
+  },
+  {
+    name: "Julio",
+    profession:
+      "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
+    mail: "villajul3@hotmail.com",
+    image: julio,
+    age: 26,
+  },
+  {
+    name: "Matias",
+    profession:
+      "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
+    mail: "mativillagra98@gmail.com",
+    image: matias,
+    age: 26,
+  },
+  {
+    name: "Ramiro",
+    profession:
+      "I am a front end developer. I provide clean code and perfect design. I also make the website more and more interactive. I apply different technologies to improve the site. Support and inquiries.",
+    mail: "ramijujuy@gmail.com",
+    image: dat,
+    age: 47,
+  },
+];
 
-    const About = () =>{
+const About = () => {
+  return (
+    <div className="container">
+        <div>
 
-          
-        
-        return (
-           
- 
-            <div>
-                <h5 className = "container h5">We are a group dedicated to developing and integrating technology in order to provide solutions to various industries and private and public organizations in different parts of Argentina.
+      <h5 className="container h5">
+        We are a group dedicated to developing and integrating technology in
+        order to provide solutions to various industries and private and public
+        organizations in different parts of Argentina. Our competitive advantage
+        is to provide added value to our clients through a professional service,
+        we offer complete solutions tailored to the needs of our clients.{" "}
+      </h5>
+        </div>
 
-Our competitive advantage is to provide added value to our clients through a professional service, we offer complete solutions tailored to the needs of our clients. </h5>
-            
-            
-            <h2 className="container dat">About Us </h2>
-            
-            
-             {nos.map(e => (          
-           
-           <section className="container about">
-           <div  className = "container main"> 
-                    
-            <img src={e.image} width="420px" height="310 px"/>
-               
-            <div className="container about-text">
+      <h2 className="container dat">About Us </h2>
 
-            <h1 className="container about-texth1">{e.name}</h1>  
-                
-             <h5 className="container about-texth5">Developer <span className="container span">& Desingner </span> </h5>
+      {nos.map((e, i) => (
+        <section key={i}>
+          <div className="container about">
+            <div className="main">
+              <img className="image" src={e.image} />
 
-             <p className="container p" >  {e.profession}     </p>
-             <button type="button" className="container button"  href={e.mail} >{e.mail} </button>
-
-
+              <div className="about">
+                <div>
+                  <h1 className="container about-texth1">{e.name}</h1>
+                </div>
+                <div>
+                  <h5 className="about-texth5">
+                    Developer <span className="span">& Desingner </span>{" "}
+                  </h5>
+                </div>
+                <div>
+                  <p className="p"> {e.profession} </p>
+                </div>
+                <div className="btn_selec">
+                  <button type="button" className="btn" href={e.mail}>
+                    {e.mail}{" "}
+                  </button>
+                </div>
+              </div>
             </div>
-            
-                        
-        
-            </div>
-            </section>
-            ))}
-            
-             
-
-            </div>
-            )
-        }
-        export default About;
+          </div>
+        </section>
+      ))}
+    </div>
+  );
+};
+export default About;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,12 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUsersById, selectUserById } from "../../store/userById";
 import { selectThisUser } from "../../store/thisUser";
 import { useAuth0 } from "@auth0/auth0-react";
-import axios from 'axios';
+import axios from "axios";
 import validate from "./validate";
 import "./Profile.css";
 
 export default function Profile({ userId }) {
   const dispatch = useDispatch();
+  
   let userData = useSelector(selectThisUser);
   const { getIdTokenClaims } = useAuth0();
   const user = useSelector(selectUserById);
@@ -65,16 +66,16 @@ export default function Profile({ userId }) {
           !input.first_name &&
           !input.last_name &&
           !input.birth_date &&
-          !input.profile_picture 
+          !input.profile_picture
         )
           return alert("No values ​​to update");
-        if(
+        if (
           error.first_name ||
           error.last_name ||
           error.birth_date ||
-          error.profile_picture 
-        ) 
-          return alert("Error in any of the fields")
+          error.profile_picture
+        )
+          return alert("Error in any of the fields");
         try {
           axios
             .put(
@@ -116,109 +117,139 @@ export default function Profile({ userId }) {
   useEffect(() => {}, [userData]);
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Profile Image: </label>
-          <img
-            className="profPic"
-            src={
-              user.profile_picture
-                ? user.profile_picture
-                : userData.userDb.profile_picture
-            }
-            alt="Profile"
-          />
-          <div className={inputHidden.edit}>
-            <input
-              className={inputHidden.profile_picture}
-              type="text"
-              name="profile_picture"
-              id="pic"
-              value={input.profile_picture}
-              onChange={handleInputChange}
+    <div>
+      <div>
+        <form className="pi" onSubmit={handleSubmit}>
+          <div className="pi">
+            <label className="pi">Profile Image: </label>
+            <img
+              className="profPic"
+              src={
+                user.profile_picture
+                  ? user.profile_picture
+                  : userData.userDb.profile_picture
+              }
+              alt="Profile"
             />
-            <button value="profile_picture" onClick={handleHidden}>
-              Change
-            </button>
           </div>
-        </div>
-        <div>
-          <label>
-            First Name:{" "}
-            {user.first_name ? user.first_name : userData.userDb.first_name}
-          </label>
-        </div>
-        <div className={inputHidden.edit}>
-          <input
-            className={inputHidden.first_name}
-            type="text"
-            name="first_name"
-            id="name"
-            value={input.first_name}
-            onChange={handleInputChange}
-          />
-          <p className="errorAlert__errorMessage">{error.first_name}</p>
-          <button value="first_name" onClick={handleHidden}>
-            Edit
+
+          <div className={inputHidden.edit}>
+            <div className="pi">
+              
+                <input
+                  className={`pi input ${inputHidden.profile_picture}`}
+                  type="text"
+                  name="profile_picture"
+                  id="pic"
+                  value={input.profile_picture}
+                  onChange={handleInputChange}
+                />
+              
+
+              <button
+                className="pi btn"
+                value="profile_picture"
+                onClick={handleHidden}
+              >
+                Change
+              </button>
+            </div>
+          </div>
+          <div className="pi">
+            <label>
+              First Name:{" "}
+              {user.first_name ? user.first_name : userData.userDb.first_name}
+            </label>
+          </div>
+            <div className={inputHidden.edit}>
+          <div className="pi">
+              <input
+                className={`pi input ${inputHidden.first_name}`}
+                type="text"
+                name="first_name"
+                id="name"
+                value={input.first_name}
+                onChange={handleInputChange}
+              />
+              <p className="errorAlert__errorMessage">{error.first_name}</p>
+              <button className="btn" value="first_name" onClick={handleHidden}>
+                Edit
+              </button>
+            </div>
+          </div>
+          <div className="pi">
+            <label>
+              Last Name:{" "}
+              {user.last_name
+                ? user.last_name
+                : userData.userDb.last_name && userData.userDb.last_name}
+            </label>
+              <div className={inputHidden.edit}>
+            <div className="pi">
+                <input
+                  className={`pi input ${inputHidden.last_name}`}
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  value={input.last_name}
+                  onChange={handleInputChange}
+                />
+                <p className="errorAlert__errorMessage">{error.last_name}</p>
+                <button
+                  className="btn"
+                  value="last_name"
+                  onClick={handleHidden}
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="pi">
+            <label>
+              Birth Date:{" "}
+              {user.birth_date
+                ? user.birth_date
+                : userData.userDb.birth_date && userData.userDb.birth_date}
+            </label>
+              <div className={inputHidden.edit}>
+            <div className="pi">
+                <input
+                  className={`pi input ${inputHidden.birth_date}`}
+                  type="date"
+                  name="birth_date"
+                  id="birth_date"
+                  value={input.birth_date}
+                  onChange={handleInputChange}
+                />
+                <p className="errorAlert__errorMessage">{error.birth_date}</p>
+                <button
+                  className="btn"
+                  value="birth_date"
+                  onClick={handleHidden}
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="pi">
+            <label>Email: {userData.userDb.email}</label>
+          </div>
+          <button
+            className={`btn ${inputHidden.button}`}
+            value="edit"
+            onClick={handleHidden}
+          >
+            Edit Profile
           </button>
-        </div>
-        <div>
-          <label>
-            Last Name:{" "}
-            {user.last_name
-              ? user.last_name
-              : userData.userDb.last_name && userData.userDb.last_name}
-          </label>
-          <div className={inputHidden.edit}>
-            <input
-              className={inputHidden.last_name}
-              type="text"
-              name="last_name"
-              id="last_name"
-              value={input.last_name}
-              onChange={handleInputChange}
-            />
-            <p className="errorAlert__errorMessage">{error.last_name}</p>
-            <button value="last_name" onClick={handleHidden}>
-              Edit
-            </button>
-          </div>
-        </div>
-        <div>
-          <label>
-            Birth Date:{" "}
-            {user.birth_date
-              ? user.birth_date
-              : userData.userDb.birth_date && userData.userDb.birth_date}
-          </label>
-          <div className={inputHidden.edit}>
-            <input
-              className={inputHidden.birth_date}
-              type="date"
-              name="birth_date"
-              id="birth_date"
-              value={input.birth_date}
-              onChange={handleInputChange}
-            />
-            <p className="errorAlert__errorMessage">{error.birth_date}</p>
-            <button value="birth_date" onClick={handleHidden}>
-              Edit
-            </button>
-          </div>
-        </div>
-        <div>
-          <label>Email: {userData.userDb.email}</label>
-        </div>
-        <button
-          className={inputHidden.button}
-          value="edit"
-          onClick={handleHidden}
-        >
-          Edit Profile
-        </button>
-        <input className={inputHidden.submit} type="submit" value="submit" />
-      </form>
-    </>
+          <input
+            className={`btn ${inputHidden.submit}`}
+            type="submit"
+            value="submit"
+          />
+        </form>
+      </div>
+    </div>
   );
 }
