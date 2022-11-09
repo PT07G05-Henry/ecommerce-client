c40880cc92d211830240b3c6869b97fefadfe43c
Profesorjand <jorge.andrade.y@hotmail.com>
2022-11-09 17:41:00 -0300

    fix SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,31 +3,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { GoSearch } from "react-icons/go";
 import { MdArrowBackIosNew } from "react-icons/md";
 import { Link, useNavigate } from "react-router-dom";
-import { getProducts, selectProducts } from "../../store/products";
+import { getProducts, selectProducts} from "../../store/products";
+import { selectAllProducts, getAllProducts } from "../../store/allProducts";
 import "./searchBar.css";
 
 export default function SearchBar() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const products = useSelector(selectProducts);
+  const products = useSelector(selectAllProducts);
   const [product, setProduct] = useState("");
   const [show, setShow] = useState(false);
   const [preview, setPreview] = useState([]);
 
   function filterPreview(e) {
+    
     setPreview([]);
     if (e.target.value === '') {
         setPreview([]);
     } else {
-        let value = e.target.value.toLowerCase()
-        for (let product of products.results) {
-            let name = product.name.toLowerCase()
-            if (name.indexOf(value) !== -1 && !preview.includes(product)) {
-                let result = [];
-                result.push(product)
-                setPreview(preview.concat(result))
-            }
-        }
+      let value = e.target.value.toLowerCase()
+      const filter = products.filter((p)=> p.name.toLowerCase().includes(value) )
+      console.log(filter)
+      setPreview(filter)
+        
+        // for (const product of products) {
+        //     let name = product.name.toLowerCase()
+        //     if (name.includes(value) && preview.indexOf(product.name) === -1) {
+        //         let result = [];
+        //         result.push(product)
+        //         setPreview(preview.concat(result))
+        //     }
+        // }
     }
   }
 
@@ -37,7 +43,8 @@ export default function SearchBar() {
   }
 
   useEffect(() => {
-    dispatch(getProducts())
+    dispatch(getProducts());
+    dispatch(getAllProducts())
   }, [])
 
 
@@ -75,8 +82,10 @@ export default function SearchBar() {
                 onChange={handleSubmit}
                 placeholder="Search products..."
               />
+              {preview.length>0 &&
+              <div className="scroll">
               {preview.map((p) => {
-                return <Link to={`/products/${p.id}`}
+                return <p><Link to={`/products/${p.id}`}
                     onClick={e => {
                         setProduct("");
                         setPreview([]);
@@ -84,7 +93,9 @@ export default function SearchBar() {
                     }}>
                     {p.name}
                 </Link>
+                </p>
               })}
+              </div>}
               <button type="submit" className="btn btn-primary">
                 Search
               </button>
